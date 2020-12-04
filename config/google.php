e29f78f60c57c8c0c92a8f3dd58bb51bc2abcf5e
selimsaral <saral_selim@hotmail.com>
2020-12-04 18:38:06 +0300

    add google credentials

---
+++ config/google.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    "type"                        => "service_account",
+    "project_id"                  => "teknofixcase",
+    "private_key_id"              => "b13e6fb8ca810f4cbd48037f383da03d39000c48",
+    "private_key"                 => "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCrRn7crrF4pySl\nQDT7ZL2IQuGL2PPpx5mWbHThg8OBUPWXTE4xjpUqTMtUjPk5qpL2M9HX8ToZ9aXu\nyH5zQvicOCErySAnW/wvduFyX0DJc+kHJ1sU+coxHh/GWNMs/gl0MBfiWM7rMlDE\nIPt2YggowBBLWsMmOW7GDTN2B3/jEwVnnW4isKbKGaS1+8t4x5WV8P6s1e2bYSun\ndMCwZmeopFWVKy/tKdX6UxPZpbHpblPz5d7AM3p9Et/yPKEH43K1Z8CyBuYpIhir\nqicRioITqrqjUx24qkkgJE2ElLZyAYD6lM54Bjmtc/CXBGZv+vZbJ4eyGQl6xXSq\nbCI2t6Z9AgMBAAECggEAJpbKwPJsImS4NTI+7bAgItn8wrs2E2NJB7L2b5P2IcXp\nm3XR/Ujkc0QboLhlc5hqpaV9v0NGpbAqu165XMNHS706vgiB4dRQAKqud0weRvVC\nWL4Ff7ea1k+09YWwSKgQ3bhzPShosGWk/Fd5CI5kS7ggQ7YWl19jxaQWRt7AMdsK\nAdl85BGXOn9+vIlytUcFM37qBZYlupOzf1/WnWwXRn+hXVnuM/L0PZ4mOdQxz72+\nmkVgswlPUJHm87etIgFA/dIycMVThIvWMBvEhOGQBw3set56LQCGhfTiYUVzqPhh\nBjXQeRPSi7l6rDd421QAoLK3shQ9XAaclmyc1QA3OQKBgQDdRbmHFN8bNCf9NNVX\nra+lnpjQCVR6Dw1T7n9e9KjKLgBlGb7krlWtN0QZYyT30aaecllKJuGR1eSP2+8k\n7UgMDuSW1OuGA/3OIytGEnvE+NDhZP5I1FxD5zWRE3SpFl/aJoCjmYpcyEvFG3TC\nhOn0FlqbTarl75LvOO8eQIuVqQKBgQDGJ/474o7CvSTyhMQlGEnqzKLVHNJCi1y2\nu385Ha43UjPWh0AtjGtt46ry0UY3DJMoXKI/mFPFVIREXW1d1pyUee0IxppI6St5\nM78rDO9MgmL055NfCWdCR9/rdm3VV5LlysRdTjWwAfv5Zm7VEaYjL0h4wVezca1a\n2489x6nmtQKBgDY8oP07YHSKlEK8cI7NRACUn2Be6RYCGoI4M4BS+AyWugIODXyA\nPv/2S/rtEmKsQOsVnE1QunnEtAGPJ08l2kMaY22VQyXWVJ/yfujuHuhpFMoqtymi\n4/rA8aBd5HqNwXD3fxCa0g9sCSaHVFdZvP7Xe94Va45w6tM4YWRI7e4BAoGBAKpM\nBqdqvuxH56/xQCztIeGDutFScrUz1htJKj5o7qemotxU4mgCzPTk+66xAhofWX0P\nl3T2MdBeIUorzOkfnvPPkVHAdSoCEhWYzktuU146lFcLilDb4P1KjftrjVsTZzDL\nwVS6+VLpcabWTJwbDeAhK7Rz3S5fnO9/0Cv1I2QdAoGBAJO11NiAYZSy7ohdxxG4\ndRYQIjESOuyI1jn86pfn4izHiW2q7DgSmUBsjkEfIdgU512kWdKGmQEipdRObZLj\nVvhcZbzAMqV5exMuY4veyAdHSLv9LkaFJnNEDVN93CXP/IFsb8EMQ1HpG1icxXct\nqpvAwk/V7Vi2AD7IY35KmHEu\n-----END PRIVATE KEY-----\n",
+    "client_email"                => "speechtotext@teknofixcase.iam.gserviceaccount.com",
+    "client_id"                   => "115917929619939938795",
+    "auth_uri"                    => "https://accounts.google.com/o/oauth2/auth",
+    "token_uri"                   => "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url" => "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url"        => "https://www.googleapis.com/robot/v1/metadata/x509/speechtotext%40teknofixcase.iam.gserviceaccount.com"
+];
