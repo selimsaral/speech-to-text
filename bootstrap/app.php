1e6b43275e632efd29ab6969781e547185c6d8e0
selimsaral <saral_selim@hotmail.com>
2020-12-05 11:34:01 +0300

    add SpeechToText Validation

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -95,6 +95,7 @@ $app->configure('google');
 // $app->register(App\Providers\AppServiceProvider::class);
 // $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
+$app->register(Anik\Form\FormRequestServiceProvider::class);
 
 /*
 |--------------------------------------------------------------------------
